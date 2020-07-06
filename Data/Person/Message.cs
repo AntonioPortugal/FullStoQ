b8e64f29fd0000b0c3d99571791e1649f9e6080c
v-vilhena <61181510+v-vilhena@users.noreply.github.com>
2020-07-06 15:46:16 +0100

    Updated Profile, Added Message

---
+++ Data/Person/Message.cs
@@ -0,0 +1,45 @@
+﻿using Recodme.RD.FullStoQ.Data.Base;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Recodme.RD.FullStoQ.Data.Person
+{
+    public class Message : Entity
+    {
+        private string _text;
+        [Required]
+        public string Text
+        {
+            get => _text;
+            set
+            {
+                _text = value;
+                RegisterChange();
+            }
+        }
+
+        [ForeignKey("ProfileSender")]
+        public Guid ProfileSenderId { get; set; }
+        public virtual Profile ProfileSender { get; set; }
+
+        [ForeignKey("ProfileReceiver")]
+        public Guid ProfileReceiverId { get; set; }
+        public virtual Profile ProfileReceiver { get; set; }
+
+        public Message(string text, Guid profileSenderId, Guid profileReceiverId) : base()
+        {
+            _text = text;
+            ProfileReceiverId = profileReceiverId;
+            ProfileSenderId = profileSenderId;
+        }
+        public Message(Guid id, DateTime createdAt, DateTime updatedAt, bool isDeleted, string text, Guid profileSenderId, Guid profileReceiverId) : base(id, createdAt, updatedAt, isDeleted)
+        {
+            _text = text;
+            ProfileReceiverId = profileReceiverId;
+            ProfileSenderId = profileSenderId;
+        }
+    }
+}
