d4e73af0e4e1c8545955138f3a7d13a404e13f3e
Analuizasilva <a.silva@recodme.pt>
2020-07-10 23:33:44 +0100

    List correction in class EstablishmentTest.

--- FullStoQTest/EstablishmentTest.cs
+++ FullStoQTest/EstablishmentTest.cs
@@ -66,29 +66,6 @@ namespace Recodme.RD.FullStoQ.FullStoQTest
         [TestMethod]
         public void TestDeleteEstablishment()
         {
-            var obReg = new RegionBusinessObject();
-            var reg = new Region("Continental");
-            obReg.Create(reg);
-
-            var objCom = new CompanyBusinessObject();
-            var com = new Company("Quitanda da dona Luzia", 123456);
-            objCom.Create(com);
-
-            var objEst = new EstablishmentBusinessObject();
-            var est = new Establishment("Rua da pitaia, numero 1234, Açores", "07:00",
-                "20:00", "Domingo", reg.Id, com.Id);
-            objEst.Create(est);
-
-            var res = objEst.Delete(est);
-
-            Assert.IsTrue(res.Success);
-        }
-        #endregion
-
-        #region List
-        [TestMethod]
-        public void TestListEstablishment()
-        {
             var boReg = new RegionBusinessObject();
             var boComp = new CompanyBusinessObject();
             var reg1 = boReg.List().Result.First();
@@ -98,15 +75,16 @@ namespace Recodme.RD.FullStoQ.FullStoQTest
             var est = new Establishment("Rua da pitaia, numero 1234, Açores", "07:00",
                 "20:00", "Domingo", reg1.Id, com1.Id);
             objEst.Create(est);
+            var res = objEst.Delete(est);
+            Assert.IsTrue(res.Success);
+        }
+        #endregion
 
-            var est2 = new Establishment("Rua da Manga, numero 10, Porto", "07:00",
-               "20:00", "Domingo", reg1.Id, com1.Id);
-            objEst.Create(est2);
-
-            var est3 = new Establishment("Rua do girassol, numero 134, Alentejo", "07:00",
-               "20:00", "Domingo", reg1.Id, com1.Id);
-            objEst.Create(est3);
-
+        #region List
+        [TestMethod]
+        public void TestListEstablishment()
+        {
+            var objEst = new EstablishmentBusinessObject();
             var resList = objEst.List();
             Assert.IsTrue(resList.Success && resList.Result.Count == 1);
         }
