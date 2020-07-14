de4abe202332cd236c03708f0e739d741ab577f6
António Portugal <vasc.antonio@gmail.com>
2020-07-14 15:13:11 +0100

    mixellaneous

--- FullStoqTest/ReservedQueueTest.cs
+++ FullStoqTest/ReservedQueueTest.cs
@@ -57,31 +57,31 @@ namespace Recodme.RD.FullStoQ.FullStoQTest
             Assert.IsTrue(resList.Success && resList.Result.Count == 1);
         }
 
-        //[TestMethod]
-        //public void TestUpdateReservedQueues()
-        //{
-        //    ContextSeeder.Seed();
-        //    var bo = new ReservedQueueBusinessObject();
-        //    var resList = bo.List();
-        //    var item = resList.Result.FirstOrDefault();
-        //    item.EstablishmentId = "another";
-        //    var resUpdate = bo.Update(item);
-        //    var resNotList = bo.ListNotDeleted().Result;
-        //    Assert.IsTrue(resUpdate.Success && resNotList.First().EstablishmentId == "another");
-        //}
+        [TestMethod]
+        public void TestUpdateReservedQueues()
+        {
+            ContextSeeder.Seed();
+            var bo = new ReservedQueueBusinessObject();
+            var resList = bo.List();
+            var item = resList.Result.FirstOrDefault();
+            item.EstablishmentId = Guid.NewGuid();
+            var resUpdate = bo.Update(item);
+            var resNotList = bo.ListNotDeleted().Result;
+            Assert.IsTrue(resUpdate.Success && resNotList.First().EstablishmentId == item.EstablishmentId);
+        }
 
-        //[TestMethod]
-        //public void TestUpdateReservedQueuenAsync()
-        //{
-        //    ContextSeeder.Seed();
-        //    var bo = new ReservedQueueBusinessObject();
-        //    var resList = bo.ListAsync().Result;
-        //    var item = resList.Result.FirstOrDefault();
-        //    item.EstablishmentId = "another";
-        //    var resUpdate = bo.UpdateAsync(item).Result;
-        //    resList = bo.ListNotDeletedAsync().Result;
-        //    Assert.IsTrue(resList.Success && resUpdate.Success && resList.Result.First().EstablishmentId == "another");
-        //}
+        [TestMethod]
+        public void TestUpdateReservedQueuenAsync()
+        {
+            ContextSeeder.Seed();
+            var bo = new ReservedQueueBusinessObject();
+            var resList = bo.ListAsync().Result;
+            var item = resList.Result.FirstOrDefault();
+            item.EstablishmentId = Guid.NewGuid();
+            var resUpdate = bo.UpdateAsync(item).Result;
+            resList = bo.ListNotDeletedAsync().Result;
+            Assert.IsTrue(resList.Success && resUpdate.Success && resList.Result.First().EstablishmentId == item.EstablishmentId);
+        }
 
         [TestMethod]
         public void TestDeleteReservedQueues()
